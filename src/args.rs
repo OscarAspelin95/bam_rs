5f8e627ecfc93e19168036291484b08460bf2709
Oscar Aspelin <oscar.aspelin@1928diagnostics.com>
2025-06-13 21:08:15 +0200

    refactor

--- src/args.rs
+++ src/args.rs
@@ -1,11 +1,11 @@
-
-
 use clap::Parser;
 
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
-#[command(long_about = "Parses a BAM file of aligned Nanopore reads, outputting heterogenous positions.")]
+#[command(
+    long_about = "Parses a BAM file of aligned Nanopore reads, outputting heterogenous positions."
+)]
 pub struct CommandArgs {
     #[arg(short, long)]
     pub bam: PathBuf,
@@ -15,9 +15,6 @@ pub struct CommandArgs {
 
     #[arg(long, default_value = "2", value_parser= clap::value_parser!(u8).range(1..10))]
     pub num_flanking_bases: u8,
-
-    #[arg(long, default_value = "5",  value_parser= clap::value_parser!(u8).range(5..20))]
-    pub min_hp_length: u8,
 
     #[arg(long, default_value = "10",  value_parser= clap::value_parser!(u8).range(1..))]
     pub min_read_depth: u8,
@@ -29,14 +26,13 @@ pub struct CommandArgs {
     pub outfile: PathBuf,
 }
 
-
-pub fn get_args() -> CommandArgs{
+pub fn get_args() -> CommandArgs {
     let args = CommandArgs::parse();
 
     // For now, have extra check for min het frequency.
-    if args.min_het_frequency > 1.0{
+    if args.min_het_frequency > 1.0 {
         panic!("Min het frequency cannot be larger than 1.0");
     }
 
-    return args
+    return args;
 }
