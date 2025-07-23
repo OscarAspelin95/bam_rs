283bcc3e41983490a2858aae9ad84c6d9266ae6a
Oscar Aspelin <oscar.aspelin@1928diagnostics.com>
2025-07-23 11:15:30 +0200

    Fix header print and other small changes

--- src/args.rs
+++ src/args.rs
@@ -7,31 +7,31 @@ use std::path::PathBuf;
     long_about = "Parses a BAM file of aligned Nanopore reads, outputting heterogenous positions."
 )]
 pub struct CommandArgs {
-    #[arg(short, long)]
+    #[arg(short, long, help = "Path to BAM file.")]
     pub bam: PathBuf,
 
-    #[arg(short, long)]
+    #[arg(short, long, help = "Path to FASTA file.")]
     pub fasta: PathBuf,
 
-    #[arg(long, default_value = "2", value_parser= clap::value_parser!(u8).range(1..10))]
+    #[arg(long, default_value = "2", value_parser= clap::value_parser!(u8).range(1..10), help="Extract this many reference nucleotides to the left and right of the heterogenous position")]
     pub num_flanking_bases: u8,
 
-    #[arg(long, default_value = "10",  value_parser= clap::value_parser!(u8).range(1..))]
+    #[arg(long, default_value = "10",  value_parser= clap::value_parser!(u8).range(1..), help = "Only consider positions with at least this depth.")]
     pub min_read_depth: u8,
 
-    #[arg(long, default_value_t = 0.9)]
+    #[arg(
+        long,
+        default_value_t = 0.1,
+        help = "Only consider heterogenous position with this fraction or more. E.g., 0.1 means including positions where >=10% of the nucleotides are non reference."
+    )]
     pub min_het_frequency: f32,
-
-    #[arg(short, long, required = true)]
-    pub outfile: PathBuf,
 }
 
 pub fn get_args() -> CommandArgs {
     let args = CommandArgs::parse();
 
-    // For now, have extra check for min het frequency.
-    if args.min_het_frequency > 1.0 {
-        panic!("Min het frequency cannot be larger than 1.0");
+    if args.min_het_frequency < 0.0 || args.min_het_frequency > 1.0 {
+        panic!("Min het frequency must be between 0.0 and 1.0");
     }
 
     return args;
